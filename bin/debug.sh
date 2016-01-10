ae27d485036113a7c1b43faf62fae37f91b445ae
Rob Chett <robchett@gmail.com>
2016-01-10 00:59:16 +0000

    Less verbose valgrind by default

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,4 +1,6 @@
 pushd test
-valgrind --tool=memcheck --num-callers=30 --log-file=php.log --leak-check=full --track-origins=yes -v ../bin/php test.php
+valgrind --tool=memcheck --num-callers=30 --log-file=php.log ../bin/php test.php
 chmod 777 php.log
 popd
+
+# --leak-check=full --track-origins=yes -v
