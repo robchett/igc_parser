fe71e574c37202565e43a517da16052561989dc4
Rob Chett <robchett@gmail.com>
2016-02-07 16:34:48 +0000

    Add tests for gridref_to_latlng

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,14 @@
 #include "gtest/gtest.h"
 #include "../convert.c"
+#include <math.h>
 
+double rounddp(double num, int dp) {
+    if (dp == 2) {
+        return roundf(num * 100) / 100;
+    } else if (dp == 3) {
+        return roundf(num * 1000) / 1000;
+    }
+}
 
 int main(int ac, char *av[]) {
     testing::InitGoogleTest(&ac, av);
@@ -8,7 +16,20 @@ int main(int ac, char *av[]) {
 }
 
 TEST (Conversion, latlng_to_gridref) {
-    ASSERT_STREQ ("TL757503", convert_latlng_to_gridref (52.123, 0.567));
-    ASSERT_STREQ ("SP693477", convert_latlng_to_gridref (52.123, -0.987));
-    ASSERT_STREQ ("TW134291", convert_latlng_to_gridref (50.123, 0.987));
+    EXPECT_STREQ ("TL757503", convert_latlng_to_gridref (52.123, 0.567));
+    EXPECT_STREQ ("SP693477", convert_latlng_to_gridref (52.123, -0.987));
+    EXPECT_STREQ ("TW134291", convert_latlng_to_gridref (50.123, 0.987));
+}
+
+TEST (Conversion, gridref_to_latlng) {
+    double lat,lng;
+    convert_gridref_to_latlng("TL757503", &lat, &lng);
+    EXPECT_DOUBLE_EQ(52.123f, rounddp(lat, 3));
+    EXPECT_DOUBLE_EQ(0.567f, rounddp(lng, 3));
+    convert_gridref_to_latlng("SP693477", &lat, &lng);
+    EXPECT_DOUBLE_EQ(52.123f, rounddp(lat, 3));
+    EXPECT_DOUBLE_EQ(-0.988f, rounddp(lng, 3));
+    convert_gridref_to_latlng("TW134291", &lat, &lng);
+    EXPECT_DOUBLE_EQ(50.123f, rounddp(lat, 3));
+    EXPECT_DOUBLE_EQ(0.986f, rounddp(lng, 3));
 }
